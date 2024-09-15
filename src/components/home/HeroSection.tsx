2d01a4188ece64715e029095de9bb4ae4b32835e
MD ABDULLAH <MD ABDULLAH>
2024-09-15 21:20:17 +0600

    change hero slider img

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -4,10 +4,9 @@ const HeroSection = () => {
       <div className="carousel w-full">
         <div id="slide1" className="carousel-item relative w-full">
           <img
+            src="https://i.ibb.co.com/N1y8n02/humphrey-muleba-LOA2m-Tj1vhc-unsplash.jpg"
             className="w-full max-h-screen object-cover"
-            src="https://i.ibb.co.com/qCJD31M/craig-lovelidge-c9wa9-EWN-fw-unsplash-1.jpg"
-            alt="craig-lovelidge-c9wa9-EWN-fw-unsplash-1"
-          ></img>
+          />
           <div className="absolute left-2 right-2 sm:left-5 sm:right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide4" className="btn btn-circle btn-sm sm:btn-md">
               ❮
@@ -33,9 +32,10 @@ const HeroSection = () => {
         </div>
         <div id="slide3" className="carousel-item relative w-full">
           <img
-            src="https://i.ibb.co.com/N1y8n02/humphrey-muleba-LOA2m-Tj1vhc-unsplash.jpg"
             className="w-full max-h-screen object-cover"
-          />
+            src="https://i.ibb.co.com/qCJD31M/craig-lovelidge-c9wa9-EWN-fw-unsplash-1.jpg"
+            alt="craig-lovelidge-c9wa9-EWN-fw-unsplash-1"
+          ></img>
           <div className="absolute left-2 right-2 sm:left-5 sm:right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
             <a href="#slide2" className="btn btn-circle btn-sm sm:btn-md">
               ❮
